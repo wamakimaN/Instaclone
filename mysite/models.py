2c7ef07c6f4e640e5f92a062c9128ccaa1cc6022
wamakimaN <wamamoses@gmail.com>
2020-02-10 10:28:38 +0300

    add models

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete = models.CASCADE)
+  bio = models.TextField(max_length = 30, blank = True)
+  website = models.CharField(max_length=30, blank =True)
+  phone_number = models.IntegerField(blank =True, null = True)
+  location = models.CharField(max_length = 30, blank =True)
+  birth_date = models.DateField(null =True, blank = True)
+  followers = models.ManyToManyField('Profile', blank =True)
+  following = models.ManyToManyField('Profile', blank =True)
+  profile_pic = models.ImageField(null =True, blank = True)
+
+class Post(models.Model):
+  profile = models.ForeignKey(Profile, null = True, blank = True)
+  title = models.CharField(max_length = 100)
+  image = models.ImageField()
+  posted_on = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+  post = models.ForeignKey('Post')
+  user = models.ForeignKey(User)
+  comment = models.CharField(max_length = 100)
+  post_on = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+  post = models.ForeignKey('Post')
+  user = models.ForeignKey(User)
