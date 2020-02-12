cd04e982002c632e2cb62331e520c9bc93262819
wamakimaN <wamamoses@gmail.com>
2020-02-12 17:53:35 +0300

    logout url for logout views to log users out

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     url(r'^signup/$',views.registration, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^insta/$', views.SiteView.as_view(), name='insta'),
-    url(r'^$', views.home_page, name='homePage')
+    url(r'^$', views.home_page, name='homePage'),
+    url(r'^logout/$', auth_views.logout, {"next_page": '/'}, name='logout'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
