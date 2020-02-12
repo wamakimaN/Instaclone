98db345344b92ad3d197e33a714151641f062365
wamakimaN <wamamoses@gmail.com>
2020-02-12 17:25:44 +0300

    add signals to create profile for users on signup

--- instagram/settings.py
+++ instagram/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,6 +126,9 @@ STATICFILES_DIRS = [
   os.path.join(BASE_DIR, "static"),
 ]
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'insta'
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-      url(r'^signup/$',views.registration, name='signup'),
+    url(r'^signup/$',views.registration, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^insta/$', views.SiteView.as_view(), name='insta'),
     url(r'^$', views.home_page, name='homePage')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
